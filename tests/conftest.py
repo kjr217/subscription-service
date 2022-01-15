aaf5fa6b38391d5e202c965251ca8f274a3bdb96
kjr217 <kjraja217@gmail.com>
2022-01-15 17:35:52 +0000

    feat: add test frame

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import time
+import constants_mainnet
+from brownie import (
+    accounts,
+    SubscriptionService
+)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def isolate_func(fn_isolation):
+    # perform a chain rewind after completing each test, to ensure proper isolation
+    # https://eth-brownie.readthedocs.io/en/v1.10.3/tests-pytest-intro.html#isolation-fixtures
+    pass
+
+@pytest.fixture
+def deployer():
+    yield accounts[1]
+
+@pytest.fixture
+def contracts(accounts):
+    sub = SubscriptionService.deploy()
---
+++ tests/test_unit.py
@@ -0,0 +1,2 @@
+import brownie
+import constants_mainnet
